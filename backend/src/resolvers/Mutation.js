b936352d7e8aa70dc6604df09bb71777f76a8826
Jorge-AC <jorgeardilac@hotmail.com>
2020-04-23 15:53:27 -0500

    Creating createItem mutation and items query from server to prisma

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,28 @@
-const mutations = {};
+const mutations = {
+  /*
+  createItem: async function(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    });
+
+    return item;
+  },
+*/
+
+//OR ANY OF BOTH WORKS THE SAME
+
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    });
+
+    return item;
+  }
+
+};
 
 module.exports = mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+/*
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+
+    return items;
+  },
+*/
+//OR. Both works the same
+  items: forwardTo('db')
+};
 
 module.exports = Query;
