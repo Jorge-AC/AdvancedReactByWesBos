9e49f4dbdd329babee1cf5d9f9e20c18e7e10bd9
Jorge-AC <jorgeardilac@hotmail.com>
2020-04-27 15:40:37 -0500

    Quering from items page and adding Item component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,43 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Item from '../components/Item';
+
+const ALL_ITEMS_QUERY = gql`
+  query {
+    items {
+      id
+      title
+      price
+      image
+      largeImage
+      description
+    }
+  }
+`;
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
 const home = props => (
-  <div>
-    <p>Home Page</p>
-  </div>
+  <Center>
+    <Query query={ALL_ITEMS_QUERY}>
+      { ({data, error, loading}) => {
+        if(loading) return <p>Loading...</p>
+        if(error) return <p>{error.message}</p>
+        return <ItemsList>{data.items.map(item => <Item item={item} key={item.id}></Item>)}</ItemsList>
+      }}
+    </Query>
+  </Center>
 )
 
 export default home;
