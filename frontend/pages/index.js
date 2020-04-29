3d428c00d2f741b5866165233b1137af98003ed1
Jorge-AC <jorgeardilac@hotmail.com>
2020-04-29 18:36:56 -0500

    Adding parameters to filters items to show based on pagination

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,10 +3,11 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from '../components/Item';
 import Pagination from '../components/Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query {
-    items {
+  query ($skip: Int, $first: Int){
+    items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -31,16 +32,19 @@ const ItemsList = styled.div`
 
 const home = props => (
   <Center>
-    <Query query={ALL_ITEMS_QUERY}>
+    <Query query={ALL_ITEMS_QUERY} variables={{
+      skip: props.query.page * perPage - perPage,
+      first: perPage
+    }}>
       { ({data, error, loading}) => {
         if(loading) return <p>Loading...</p>
         if(error) return <p>{error.message}</p>
         return <div>
-          <Pagination page={props.query.page}/>
+          <Pagination page={props.query.page || 1}/>
           <ItemsList>
             {data.items.map(item => <Item item={item} key={item.id}></Item>)}
           </ItemsList>
-          <Pagination page={props.query.page}/>
+          <Pagination page={props.query.page || 1}/>
         </div>
       }}
     </Query>
