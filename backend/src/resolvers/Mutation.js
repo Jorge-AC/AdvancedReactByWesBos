939ea832c4946e01c21f3beeb4f84b20d3e9e361
Jorge-AC <jorgeardilac@hotmail.com>
2020-05-06 19:19:41 -0500

    updating Item type to have user connection

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -18,8 +18,15 @@ const mutations = {
   */
   //OR ANY OF BOTH WORKS THE SAME
   async createItem(parent, args, ctx, info) {
+    if(!ctx.request.userId) throw new Error('You must be logged in to create an Item');
+
     const item = await ctx.db.mutation.createItem({
       data: {
+        user: {
+          connect: {
+            id: ctx.request.userId
+          }
+        },
         ...args
       }
     }, info);
