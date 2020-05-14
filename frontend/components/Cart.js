aa65824e48bc5dfb37126b07521f59a2bd0c29d4
Jorge-AC <jorgeardilac@hotmail.com>
2020-05-14 17:46:07 -0500

    Adding react-adopt to compose Cart render props

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,5 +1,6 @@
 import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import SickButton from './styles/SickButton';
@@ -21,38 +22,37 @@ const CART_STATE_TOGGLE = gql`
   }
 `;
 
-const Cart = props => {
-  return (
-    <User>
-      { (me) => {
-        if (!me) return null;
-        const cart = me.cart;
-        return <Mutation mutation={ CART_STATE_TOGGLE }>
-          { (toogleCart) => (
-            <Query query={ CART_STATE_QUERY }>
-              { ({ data }) => (
-                <CartStyles open={ data.cartOpen }>
-                  <header>
-                    <CloseButton onClick={ toogleCart } title="Close">&times;</CloseButton>
-                    <Supreme>Your Cart</Supreme>
-                    <p>You have { cart.length } item{ cart.length === 1 ? '' : 's' } in your cart</p>
-                    <ul>
-                      { cart.map(cartItem => <CartItem key={ cartItem.id } cartItem={ cartItem } />) }
-                    </ul>
-                  </header>
-                  <footer>
-                    <p>{ formatMoney(calcTotalPrice(cart)) }</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
-              ) }
-            </Query>
-          ) }
-        </Mutation>
-      } }
-    </User>
-  )
-}
+const Compose = adopt({
+  me: ({ render }) => <User>{ render }</User>,
+  toogleCart: ({ render }) => <Mutation mutation={ CART_STATE_TOGGLE }>{ render }</Mutation>,
+  cartOpen: ({ render }) => <Query query={ CART_STATE_QUERY }>{ render }</Query>
+})
+
+const Cart = props => (
+  <Compose>
+    { ({ me, toogleCart, cartOpen }) => {
+      if (!me) return null;
+      const cart = me.cart;
+      return (
+        <CartStyles open={ cartOpen.data.cartOpen }>
+          <header>
+            <CloseButton onClick={ toogleCart } title="Close">&times;</CloseButton>
+            <Supreme>Your Cart</Supreme>
+            <p>You have { cart.length } item{ cart.length === 1 ? '' : 's' } in your cart</p>
+            <ul>
+              { cart.map(cartItem => <CartItem key={ cartItem.id } cartItem={ cartItem } />) }
+            </ul>
+          </header>
+          <footer>
+            <p>{ formatMoney(calcTotalPrice(cart)) }</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
+      )
+    } }
+  </Compose>
+)
+
 
 export default Cart;
 export { CART_STATE_QUERY };
